19eeccc95a0b4c24a91563c585b5b4b249801875
MrHussnainAhmad <mrhussnainahmad@gmail.com>
2025-12-11 19:59:15 +0500

    added urdu font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,4 +2,11 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Noto Nastaliq Urdu"', 'ui-sans-serif', 'system-ui', 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+      },
+    },
+  },
 } satisfies Config;
